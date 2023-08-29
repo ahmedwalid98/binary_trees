78c98c3413e7715c23696db0a73034dc2ca29a0f
ahmed walid <ahmedwalid98.aw@gmail.com>
2023-08-30 00:42:28 +0300

    third task

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,4 +1,19 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_insert_right - inserts a node as the
+ *					right-child of another node
+ *
+ * @parent: is a pointer to the node to insert the right-child in
+ * @value: is the value to store in the new node
+ *
+ * Description: If parent already has a right-child, the new node
+ *				must take its place, and the old right-child must
+ *				be set as the right-child of the new node.
+ *
+ * Return: a pointer to the created node, or
+ *			NULL on failure or
+ *			if parent is NULL
+ */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
@@ -15,9 +30,5 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	node->right = parent->right;
 	node->left = NULL;
 	parent->right = node;
-
-	// if (node->right)
-	// 	node->right->parent = node;
-
 	return (node);
 }
