3630a90a5b10f503b01c372db39af7578fe43d3a
ahmed walid <ahmedwalid98.aw@gmail.com>
2023-08-30 00:29:00 +0300

    second task

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node;
+
+	if (!parent)
+		return (NULL);
+
+	node = malloc(sizeof(binary_tree_t));
+	if (node == NULL)
+		return (NULL);
+	
+	node->n = value;
+	node->parent = parent;
+	node->left = parent->left;
+	node->right = NULL;
+	parent->left = node;
+
+	if (node->left)
+		node->left->parent = node;
+	
+	return (node);
+}
